011897c0b53cd40b8ca40724bdf1160c545315de
LanderDhaen <lander.dhaen@student.hogent.be>
2026-09-08 16:15:19 +0200

    feat(event): Improved the tuple type

---
+++ apps/server/src/types/geo.d.ts
@@ -0,0 +1,8 @@
+export type Point = [longitude: number, latitude: number]
+export type Bounds = [
+  minLng: number,
+  minLat: number,
+  maxLng: number,
+  maxLat: number,
+]
+export type Path = Point[]
--- apps/server/src/types/geo.ts
+++
@@ -1,3 +0,0 @@
-export type Point = [number, number]
-export type Bounds = [number, number, number, number]
-export type Path = Point[]
