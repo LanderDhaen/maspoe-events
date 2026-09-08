ed1969b8b9494acf4401cf646c37efd089d26d11
LanderDhaen <lander.dhaen@student.hogent.be>
2026-09-09 01:21:55 +0200

    fix(event): Improved the typesafety

--- apps/server/src/lib/geo.ts
+++ apps/server/src/lib/geo.ts
@@ -6,16 +6,16 @@ export const point = ({ x, y }: Point) => sql<Point>`point(${x},${y})`
 export const path = (points: Point[]) =>
   sql<Point[]>`${`[${points.map(({ x, y }) => `(${x},${y})`).join(",")}]`}`
 
-export const calculateBoundingBox = (points: Point[]) => {
+export const calculateBoundingBox = (
+  points: Point[]
+): [number, number, number, number] => {
   const longitudes = points.map((point) => point.x)
   const latitudes = points.map((point) => point.y)
 
-  const bounds = [
+  return [
     Math.min(...longitudes),
     Math.min(...latitudes),
     Math.max(...longitudes),
     Math.max(...latitudes),
-  ] satisfies [number, number, number, number]
-
-  return bounds
+  ]
 }
