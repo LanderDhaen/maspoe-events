75b4eff507ed6bd10516b0ef4bc67e1f6dc65f88
LanderDhaen <lander.dhaen@student.hogent.be>
2026-09-09 17:28:03 +0200

    feat(hide-tracks): Update the map based on visible tracks from URL

--- apps/web/components/event-map.tsx
+++ apps/web/components/event-map.tsx
@@ -8,6 +8,7 @@ import {
   MapMarker,
   MarkerContent,
 } from "@workspace/ui/components/map"
+import { parseAsArrayOf, parseAsString, useQueryState } from "nuqs"
 import { Fragment } from "react"
 
 interface EventMapProps {
@@ -16,6 +17,13 @@ interface EventMapProps {
 }
 
 export default function EventMap({ tracks, bounds }: EventMapProps) {
+  const defaultTracks = tracks.map((track) => track.slug)
+
+  const [visibleTracks] = useQueryState(
+    "tracks",
+    parseAsArrayOf(parseAsString, ",").withDefault(defaultTracks)
+  )
+
   return (
     <Map
       bounds={bounds}
@@ -23,53 +31,55 @@ export default function EventMap({ tracks, bounds }: EventMapProps) {
         padding: 50,
       }}
     >
-      {tracks.map((track) => (
-        <Fragment key={track.slug}>
-          <MapRoute
-            key={track.slug}
-            coordinates={track.path.map((point) => [point.x, point.y])}
-            color={`#${track.color}`}
-          />
-          <MapMarker
-            key={`${track.slug}-start`}
-            longitude={track.startingPoint.x}
-            latitude={track.startingPoint.y}
-          >
-            <MarkerContent>
-              <Badge style={{ backgroundColor: `#${track.color}` }}>
-                Start
-              </Badge>
-            </MarkerContent>
-          </MapMarker>{" "}
-          <MapMarker
-            key={`${track.slug}-end`}
-            longitude={track.endPoint.x}
-            latitude={track.endPoint.y}
-          >
-            <MarkerContent>
-              <Badge style={{ backgroundColor: `#${track.color}` }}>
-                Finish
-              </Badge>
-            </MarkerContent>
-          </MapMarker>
-          {track.checkpoints.map((checkpoint) => (
+      {tracks
+        .filter((track) => visibleTracks.includes(track.slug))
+        .map((track) => (
+          <Fragment key={track.slug}>
+            <MapRoute
+              key={track.slug}
+              coordinates={track.path.map((point) => [point.x, point.y])}
+              color={`#${track.color}`}
+            />
             <MapMarker
-              key={checkpoint.id}
-              longitude={checkpoint.point.x}
-              latitude={checkpoint.point.y}
+              key={`${track.slug}-start`}
+              longitude={track.startingPoint.x}
+              latitude={track.startingPoint.y}
             >
               <MarkerContent>
-                <div
-                  className="flex size-6 items-center justify-center rounded-full text-primary-foreground tabular-nums"
-                  style={{ backgroundColor: `#${track.color}` }}
-                >
-                  {checkpoint.abbreviation}
-                </div>
+                <Badge style={{ backgroundColor: `#${track.color}` }}>
+                  Start
+                </Badge>
+              </MarkerContent>
+            </MapMarker>{" "}
+            <MapMarker
+              key={`${track.slug}-end`}
+              longitude={track.endPoint.x}
+              latitude={track.endPoint.y}
+            >
+              <MarkerContent>
+                <Badge style={{ backgroundColor: `#${track.color}` }}>
+                  Finish
+                </Badge>
               </MarkerContent>
             </MapMarker>
-          ))}
-        </Fragment>
-      ))}
+            {track.checkpoints.map((checkpoint) => (
+              <MapMarker
+                key={checkpoint.id}
+                longitude={checkpoint.point.x}
+                latitude={checkpoint.point.y}
+              >
+                <MarkerContent>
+                  <div
+                    className="flex size-6 items-center justify-center rounded-full text-primary-foreground tabular-nums"
+                    style={{ backgroundColor: `#${track.color}` }}
+                  >
+                    {checkpoint.abbreviation}
+                  </div>
+                </MarkerContent>
+              </MapMarker>
+            ))}
+          </Fragment>
+        ))}
     </Map>
   )
 }
