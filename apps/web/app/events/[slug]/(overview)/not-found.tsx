eea6ba3b8cf4e09d39da6a0d2de85ac09e045404
LanderDhaen <lander.dhaen@student.hogent.be>
2026-09-09 17:44:08 +0200

    feat(hide-tracks): Added icons to other empty states

--- apps/web/app/events/[slug]/(overview)/not-found.tsx
+++ apps/web/app/events/[slug]/(overview)/not-found.tsx
@@ -2,13 +2,18 @@ import {
   Empty,
   EmptyDescription,
   EmptyHeader,
+  EmptyMedia,
   EmptyTitle,
 } from "@workspace/ui/components/empty"
+import { CalendarX2 } from "lucide-react"
 
 export default function NotFound() {
   return (
     <Empty className="h-lvh bg-muted/30">
       <EmptyHeader>
+        <EmptyMedia variant="icon">
+          <CalendarX2 />
+        </EmptyMedia>
         <EmptyTitle>Evenement niet gevonden</EmptyTitle>
         <EmptyDescription>
           Helaas, het evenement dat je zoekt bestaat niet.
--- apps/web/app/events/[slug]/(overview)/page.tsx
+++ apps/web/app/events/[slug]/(overview)/page.tsx
@@ -17,7 +17,7 @@ import {
 import NotFound from "./not-found"
 import TrackVisibility from "@web/components/track-visibility"
 import EventMap from "@web/components/event-map"
-import { MapPinOff } from "lucide-react"
+import { CircleAlert, MapPinOff } from "lucide-react"
 
 export default async function Page({
   params,
@@ -40,6 +40,9 @@ export default async function Page({
     return (
       <Empty className="h-lvh bg-muted/30">
         <EmptyHeader>
+          <EmptyMedia variant="icon">
+            <CircleAlert />
+          </EmptyMedia>
           <EmptyTitle>Er is iets misgegaan</EmptyTitle>
           <EmptyDescription>
             Helaas, er is iets misgegaan bij het ophalen van het evenement.
